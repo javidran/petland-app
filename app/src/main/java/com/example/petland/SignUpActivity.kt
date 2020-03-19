9f917bf2b62e7decfddd674326e56b269931fc43
ariadna <ariadnavc98@gmail.com>
2020-03-19 11:53:29 +0100

    Calendario
    cambios en la fecha de nacimiento y bordes en boton sign in de la vista sign up

--- app/src/main/java/com/example/petland/SignUpActivity.kt
+++ app/src/main/java/com/example/petland/SignUpActivity.kt
@@ -1,16 +1,49 @@
 package com.example.petland
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.startActivity
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        val textView: TextView = findViewById(R.id.editTextBirthday)
+        textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+        var cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            textView.text = sdf.format(cal.time)
+
+        }
+
+        textView.setOnClickListener {
+            DatePickerDialog(this@SignUpActivity, dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
+
 
     fun signIn(view: View) {
         val intent = Intent(this, SignInActivity::class.java).apply {
