e5f9b556faa67c631af90554b18c60ba414c6f12
ariadna <ariadnavc98@gmail.com>
2020-03-20 19:40:41 +0100

    Vista Principal
    Añadida la vista principal

--- app/src/main/java/com/example/petland/MainActivity.kt
+++ app/src/main/java/com/example/petland/MainActivity.kt
@@ -9,8 +9,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        setContentView(R.layout.activity_main)
     }
+    fun signUp(view: View) {
+        val intent = Intent(this, SignUpActivity::class.java).apply {
+        }
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+    fun signIn(view: View) {
+        val intent = Intent(this, SignInActivity::class.java).apply {
+        }
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
 
 
 
