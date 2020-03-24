7701c542cd610974137cd4b1a2b0fed0adfc96f8
ariadna <ariadnavc98@gmail.com>
2020-03-24 10:06:13 +0100

    Creado alert dialog

--- app/src/main/java/com/example/petland/SignInActivity.kt
+++ app/src/main/java/com/example/petland/SignInActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.parse.ParseUser
 import kotlinx.android.synthetic.main.activity_signin.*
@@ -14,7 +16,8 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
-        }
+
+    }
 
     fun signUp(view: View) {
         val intent = Intent(this, SignUpActivity::class.java).apply {
@@ -25,6 +28,7 @@ class SignInActivity : AppCompatActivity() {
     fun login(view: View) {
        val intent = Intent(this, MenuActivity::class.java).apply {
        }
+
         ParseUser.logInInBackground(editTextUsername.text.toString(), editTextPassword.text.toString()) { user, e ->
             if (user != null) {
                 Log.d("Login", "El usuario ha entrado correctamente")
@@ -32,8 +36,18 @@ class SignInActivity : AppCompatActivity() {
                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
             } else {
                 Log.d("Login", "No existe usuario")
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Usuario incorrecto")
+                builder.setMessage("No existe usuario o contraseña incorrecta")
+                builder.setNeutralButton("Cancel"){dialogInterface , which ->
+                    Toast.makeText(applicationContext,"",Toast.LENGTH_LONG).show()
+                }
+                val alertDialog: AlertDialog = builder.create()
+                alertDialog.show()
+
+                }
+
             }
-        }
 
     }
 
