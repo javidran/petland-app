4ce506694bde8ee79faad5f4a5cd5aa606cc5cc2
Jan <Jan.escorza99#gmail.com>
2020-04-02 13:00:57 +0200

    Vista que ves cuando te registras y entras por primera vez a la app

---
+++ app/src/main/java/com/example/petland/GetFirstPetActivity.kt
@@ -0,0 +1,24 @@
+package com.example.petland
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class GetFirstPetActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_first_pet)
+    }
+    /*
+    fun createFirstPet(view: View) {
+        val intent = Intent(this, createFirstPetActivity::class.java).apply {
+        }
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
+     */
+
+}
