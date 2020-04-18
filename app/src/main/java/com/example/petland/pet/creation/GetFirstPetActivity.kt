6e1f3a88afec932837be2997bf8ed3885009a7d2
Jan <Jan.escorza99#gmail.com>
2020-04-18 22:17:11 +0200

    Añadido boton de logout y funcion a ventana de añadir primera pet

--- app/src/main/java/com/example/petland/pet/creation/GetFirstPetActivity.kt
+++ app/src/main/java/com/example/petland/pet/creation/GetFirstPetActivity.kt
@@ -2,10 +2,14 @@ package com.example.petland.pet.creation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petland.HomeActivity
 import com.example.petland.R
+import com.example.petland.sign.BootActivity
+import com.parse.ParseUser
 
 class GetFirstPetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +37,22 @@ class GetFirstPetActivity : AppCompatActivity() {
             R.anim.slide_in_right,
             R.anim.slide_out_left
         )
+    }
+
+    fun logOut(view: View){
+        val currentUser = ParseUser.getCurrentUser()
+        if (currentUser != null) {
+            //Log.d(HomeActivity.TAG, getString(R.string.loggedOut)) //Mensaje en logcat
+            ParseUser.logOut()
+            Toast.makeText(this, getString(R.string.loggedOut), Toast.LENGTH_SHORT).show()
+            val intent = Intent(
+                this,
+                BootActivity::class.java
+            ).apply { //Para pasar de esta vista, de nuevo al SignIn
+            }
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
     }
 
 }
