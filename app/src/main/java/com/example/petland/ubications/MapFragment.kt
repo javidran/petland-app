78f54fd7bb410634d398d37c85b6510c8a937f28
ariadna <ariadnavc98@gmail.com>
2020-05-01 12:37:59 +0200

    commit intentando geolocalizar

--- app/src/main/java/com/example/petland/ubications/MapFragment.kt
+++ app/src/main/java/com/example/petland/ubications/MapFragment.kt
@@ -1,24 +1,40 @@
 package com.example.petland.ubications
 
+import android.app.Activity
 import android.app.PendingIntent.getActivity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.petland.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback,
+    GoogleMap.OnMarkerClickListener{
+    private lateinit var map: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,15 +45,43 @@ class MapFragment : Fragment() {
             childFragmentManager.findFragmentById(R.id.frg) as SupportMapFragment
         mapFragment.getMapAsync { mMap ->
             mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-            val sydney = LatLng(-34.0, 151.0)
-            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         }
         return rootView
     }
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this.requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
 
+        // 1
+        map.isMyLocationEnabled = true
+
+// 2
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            // 3
+            lastLocation = location
+           val currentLatLng = LatLng(location.latitude, location.longitude)
+
+            map.animateCamera(newLatLngZoom(currentLatLng, 12f))
+        }
+    }
+
+
+
+    override fun onMapReady(p0: GoogleMap?) {
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+        setUpMap()
+    }
+
+    override fun onMarkerClick(p0: Marker?) = false
 
     companion object {
-
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
         @JvmStatic
         fun newInstance() =
             MapFragment().apply {}
