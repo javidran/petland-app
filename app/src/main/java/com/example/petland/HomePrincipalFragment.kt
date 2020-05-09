a11b1267cb4985c42dc0c6acde5d6495af5d8bec
ariadna <ariadnavc98@gmail.com>
2020-05-09 19:56:09 +0200

    cambios de fragment finalizacion

--- app/src/main/java/com/example/petland/HomePrincipalFragment.kt
+++ app/src/main/java/com/example/petland/HomePrincipalFragment.kt
@@ -6,10 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.petland.image.ImageUtils
+import com.example.petland.mapas.MapsFragment
 import com.example.petland.pet.Pets.Companion.getSelectedPet
+import kotlinx.android.synthetic.main.content_home.*
+import kotlinx.android.synthetic.main.fragment_home_principal.view.*
 import kotlinx.android.synthetic.main.fragment_user_profile.view.*
+import kotlinx.android.synthetic.main.fragment_user_profile.view.profileImage
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -17,6 +22,7 @@ class HomePrincipalFragment : Fragment() {
     private val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
     private lateinit var layoutManager: LinearLayoutManager
     private lateinit var rootView: View
+    lateinit var fragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +37,7 @@ class HomePrincipalFragment : Fragment() {
     ): View? {
 
        rootView = inflater.inflate(R.layout.fragment_home_principal, container, false)
+        rootView.iniciarPaseoImage.setOnClickListener { iniciarPaseo() }
 
         return rootView
     }
@@ -52,7 +59,13 @@ class HomePrincipalFragment : Fragment() {
         ImageUtils.retrieveImage(pet, rootView.profileImage)
     }
 
+ fun iniciarPaseo() {
 
+     val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+     fragment = MapsFragment.newInstance()
+     transaction.replace(R.id.frameLayout, fragment)
+     transaction.commit()
+ }
     companion object {
 
         @JvmStatic
