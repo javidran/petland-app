c5a6ab4c3affef77d5bfed130aaccfa317bb4e2d
ariadna <ariadnavc98@gmail.com>
2020-05-11 17:27:12 +0200

    añadidos eventos en frag principal

--- app/src/main/java/com/example/petland/HomePrincipalFragment.kt
+++ app/src/main/java/com/example/petland/HomePrincipalFragment.kt
@@ -1,5 +1,6 @@
 package com.example.petland
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,16 +8,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.petland.events.model.PetEvent
+import com.example.petland.events.ui.EventAdapter
+import com.example.petland.events.ui.ViewEventActivity
+import com.example.petland.events.ui.ViewEventCallback
+import com.example.petland.events.ui.creation.CreateEventActivity
 import com.example.petland.image.ImageUtils
 import com.example.petland.pet.Pets.Companion.getSelectedPet
-import kotlinx.android.synthetic.main.fragment_user_profile.view.*
+import kotlinx.android.synthetic.main.fragment_home_principal.view.*
+import kotlinx.android.synthetic.main.fragment_user_profile.view.profileImage
 import java.text.SimpleDateFormat
 import java.util.*
 
-class HomePrincipalFragment : Fragment() {
+class HomePrincipalFragment : Fragment(), ViewEventCallback {
     private val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
     private lateinit var layoutManager: LinearLayoutManager
     private lateinit var rootView: View
+    private lateinit var adapter: EventAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +39,14 @@ class HomePrincipalFragment : Fragment() {
     ): View? {
 
        rootView = inflater.inflate(R.layout.fragment_home_principal, container, false)
-
+        rootView.recyclerViewEvents.layoutManager = LinearLayoutManager(context)
         return rootView
     }
 
     override fun onResume() {
         super.onResume()
+        adapter = EventAdapter(PetEvent.getEventsFromPet(),  context!!, this)
+        rootView.recyclerViewEvents.adapter = adapter
         setPetInfo()
     }
     private fun setPetInfo() {
@@ -58,5 +68,11 @@ class HomePrincipalFragment : Fragment() {
         @JvmStatic
         fun newInstance() =
            HomePrincipalFragment().apply {}
+    }
+
+    override fun startViewEventActivity(event: PetEvent) {
+        val intent = Intent(context, ViewEventActivity::class.java).apply {}
+        intent.putExtra("event", event)
+        startActivity(intent)
     }
 }
